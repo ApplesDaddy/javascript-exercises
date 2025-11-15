fae4b10db12deee39ab7d745c9a3737230de7838
Francis Ferrer <francismjferrer@gmail.com>
2025-11-14 22:36:46 -0800

    finish ex6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    // If `num` is a negative number, return the string `'ERROR'` instead.
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let res = "";
+    for(let i = 0; i < num; i++){
+        res += string;
+    }
+    return res;
+    // repeatString('hey', 3) // returns 'heyheyhey'
+    // This function will take two arguments, `string` and `num`.
 };
 
 // Do not edit below this line
